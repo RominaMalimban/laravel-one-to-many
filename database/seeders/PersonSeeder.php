defa0dfc78d91bf8c110167fac194c24187b5fec
RominaMalimban <romina.lyn@hotmail.it>
2023-02-13 22:31:48 +0100

    creazione dati fake per People

--- database/seeders/PersonSeeder.php
+++ database/seeders/PersonSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Person;
 class PersonSeeder extends Seeder
 {
     /**
@@ -14,6 +14,6 @@ class PersonSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Person::factory()-> count(50)-> create();
     }
 }
