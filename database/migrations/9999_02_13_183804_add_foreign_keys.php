7081f5de4286478031be29289940517a2963492f
RominaMalimban <romina.lyn@hotmail.it>
2023-02-13 19:55:24 +0100

    creazione foreign keys migrations

---
+++ database/migrations/9999_02_13_183804_add_foreign_keys.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // POSTS:
+        Schema::table('posts', function (Blueprint $table) {
+
+            // VERSIONE 1:
+            // $table -> bigInteger('person_id') ->unsigned();
+
+            // $table  -> foreign('person_id')
+            //         ->references('id')
+            //         ->on('people');
+
+            // VERSIONE 2 COMPATTA:
+            $table  -> foreignId('person_id')
+                    ->constrained();
+        });
+
+        // PERSON_DETAILS:
+        Schema::table('person_details', function (Blueprint $table) {
+            
+            // qui uso versione 2:
+            $table  -> foreignId('person_id')
+                    ->constrained();
+            
+            // primary key rapporto 1to1:
+            $table -> primary('person_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
