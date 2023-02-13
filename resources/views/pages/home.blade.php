09c68bd366602e3d357920f6c1759e90bae05d3e
RominaMalimban <romina.lyn@hotmail.it>
2023-02-14 00:09:27 +0100

    stampa su pagina

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h2>home</h2>
+    <h2>People</h2>
+
+    <ul>
+        @foreach ($people as $person)
+            <li>
+                {{-- Person --}}
+                <div>
+                    <strong>Username:</strong> {{ $person -> name}}
+                </div>
+                <div>
+                    <strong>Email:</strong> {{$person -> mail}}
+                </div>
+
+                {{-- Person Detail --}}
+                <div>
+                    <strong>Phone:</strong> {{ $person -> personDetail -> phone}}
+                </div>
+                <div>
+                    <strong>Address:</strong> {{ $person -> personDetail -> address}}
+                </div>
+
+                <hr>
+
+                <h4>Number of posts: {{$person -> posts -> count()}}</h4>
+                @foreach ($person -> posts as $post)
+                    <h5>{{$post -> title}}</h5>
+                    <p>{{$post -> body}}</p>
+                @endforeach
+            </li> 
+            <hr>   
+        @endforeach
+    </ul>
 @endsection
